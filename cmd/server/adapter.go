de41c37e5cd3945ec461d11bc329924cea4fffe1
Alexey Plenkin <aveplen@yandex.ru>
2023-05-27 21:11:20 +0300

    add logging

--- cmd/server/adapter.go
+++ cmd/server/adapter.go
@@ -7,6 +7,19 @@ import (
 	"github.com/simonvetter/modbus"
 )
 
+const (
+	LogRequestSeparatorSymbol = "="
+	LogRequestSeparatorLength = 80
+)
+
+var LogRequestSeparator = func() string {
+	buf := ""
+	for i := 0; i < LogRequestSeparatorLength; i++ {
+		buf += LogRequestSeparatorSymbol
+	}
+	return buf
+}()
+
 type AdapterHandler struct {
 	handler *ModbusHandler
 }
@@ -25,36 +38,21 @@ func NewAdapterHandler(handler *ModbusHandler) *AdapterHandler {
 // - res:	coil values (only for reads)
 // - err:	either nil if no error occurred, a modbus error
 func (h *AdapterHandler) HandleCoils(req *modbus.CoilsRequest) ([]bool, error) {
-	if req.IsWrite && req.Quantity == 1 {
-		log.Printf(
-			"Function 0x05 (write single coil) accessed with Addr: %d, Arguments: %v",
-			req.Addr, req.Args,
-		)
+	log.Print(LogRequestSeparator)
 
+	if req.IsWrite && req.Quantity == 1 {
 		if err := h.handler.WriteSingleCoil0x05(req.Addr, req.Args[0]); err != nil {
 			return nil, fmt.Errorf("handle coils: %w", err)
 		}
-
 		return nil, nil
 	}
 
 	if req.IsWrite {
-		log.Printf(
-			"Function 0x0F (write multiple coils) accessed with Addr: %d, Quantity: %d, Arguments: %v",
-			req.Addr, req.Quantity, req.Args,
-		)
-
 		if err := h.handler.WriteMultipleCoils0x0F(req.Addr, req.Args[:req.Quantity]); err != nil {
 			return nil, fmt.Errorf("handle coils: %w", err)
 		}
-
 		return nil, nil
 	}
-
-	log.Printf(
-		"Funtion 0x01 (read coils) accessed with Addr: %d, Quantity: %d",
-		req.Addr, req.Quantity,
-	)
 
 	coils, err := h.handler.ReadCoils0x01(req.Addr, int(req.Quantity))
 	if err != nil {
@@ -68,10 +66,13 @@ func (h *AdapterHandler) HandleCoils(req *modbus.CoilsRequest) ([]bool, error) {
 // - res: discrete input values
 // - err:	either nil if no error occurred, a modbus error
 func (h *AdapterHandler) HandleDiscreteInputs(req *modbus.DiscreteInputsRequest) ([]bool, error) {
+	log.Print(LogRequestSeparator)
+
 	inputs, err := h.handler.ReadDiscreteInputs0x02(req.Addr, int(req.Quantity))
 	if err != nil {
 		return nil, fmt.Errorf("handle discrete inputs: %w", err)
 	}
+
 	return inputs, nil
 }
 
@@ -81,12 +82,9 @@ func (h *AdapterHandler) HandleDiscreteInputs(req *modbus.DiscreteInputsRequest)
 // - res:	register values
 // - err:	either nil if no error occurred, a modbus error
 func (h *AdapterHandler) HandleHoldingRegisters(req *modbus.HoldingRegistersRequest) ([]uint16, error) {
-	if req.IsWrite && req.Quantity == 1 {
-		log.Printf(
-			"Function 0x06 (write single register) accessed with Addr: %d, Quantity: %d, Arguments: %v",
-			req.Addr, req.Quantity, req.Args,
-		)
+	log.Print(LogRequestSeparator)
 
+	if req.IsWrite && req.Quantity == 1 {
 		if err := h.handler.WriteSingleRegister0x06(req.Addr, req.Args[0]); err != nil {
 			return nil, fmt.Errorf("handle holding registers: %w", err)
 		}
@@ -95,22 +93,12 @@ func (h *AdapterHandler) HandleHoldingRegisters(req *modbus.HoldingRegistersRequ
 	}
 
 	if req.IsWrite {
-		log.Printf(
-			"Function 0x10 (write mutliple registers) accessed with Addr: %d, Quantity: %d, Arguments: %v",
-			req.Addr, req.Quantity, req.Args,
-		)
-
 		if err := h.handler.WriteMultipleRegisters0x10(req.Addr, req.Args[:req.Quantity]); err != nil {
 			return nil, fmt.Errorf("handle holding registers: %w", err)
 		}
 
 		return nil, nil
 	}
-
-	log.Printf(
-		"Function 0x03 (read holding registers) accessed with Addr: %d, Quantity: %d",
-		req.Addr, req.Quantity,
-	)
 
 	regs, err := h.handler.ReadHoldingRegisters0x03(req.Addr, int(req.Quantity))
 	if err != nil {
@@ -125,10 +113,12 @@ func (h *AdapterHandler) HandleHoldingRegisters(req *modbus.HoldingRegistersRequ
 // - res:	register values
 // - err:	either nil if no error occurred, a modbus error
 func (h *AdapterHandler) HandleInputRegisters(req *modbus.InputRegistersRequest) ([]uint16, error) {
-	log.Printf("Function 0x04 (read input registers) at Addr: %d with Quantity: %d", req.Addr, req.Quantity)
+	log.Print(LogRequestSeparator)
+
 	regs, err := h.handler.ReadInputRegisters0x04(req.Addr, int(req.Quantity))
 	if err != nil {
 		return nil, fmt.Errorf("handle input registers: %w", err)
 	}
+
 	return regs, nil
 }
--- cmd/server/handler.go
+++ cmd/server/handler.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 type ModbusHandler struct {
@@ -15,101 +16,129 @@ func NewModbusHandler(service *ModbusService) *ModbusHandler {
 }
 
 func (h *ModbusHandler) ReadCoils0x01(addr uint16, cnt int) ([]bool, error) {
-	result := make([]bool, 0, cnt)
+	log.Printf("Call function 0x01 (read coils), addr: %d, cnt: %d", addr, cnt)
 
+	result := make([]bool, 0, cnt)
 	for a := addr; a < addr+uint16(cnt); a++ {
 		coil, err := h.service.GetCoil(a)
 		if err != nil {
+			log.Printf("Could not get coil at addr: %d, reason: %v", addr, err)
 			return nil, fmt.Errorf("get coil at address %d: %w", a, err)
 		}
 
 		result = append(result, coil)
 	}
 
+	log.Printf("Successfuly read %d coils at addr: %d", cnt, addr)
 	return result, nil
 }
 
 func (h *ModbusHandler) ReadDiscreteInputs0x02(addr uint16, cnt int) ([]bool, error) {
-	result := make([]bool, 0, cnt)
+	log.Printf("Call function 0x02 (read discrete inputs), addr: %d, cnt: %d", addr, cnt)
 
+	result := make([]bool, 0, cnt)
 	for a := addr; a < addr+uint16(cnt); a++ {
 		dis, err := h.service.GetDiscreteInputs(a)
 		if err != nil {
+			log.Printf("Could not get discrete input at addr: %d, reason: %v", addr, err)
 			return nil, fmt.Errorf("get input register at address %d: %w", a, err)
 		}
 
 		result = append(result, dis)
 	}
 
+	log.Printf("Successfuly read %d discrete inputs at addr: %d", cnt, addr)
 	return result, nil
 }
 
 func (h *ModbusHandler) ReadHoldingRegisters0x03(addr uint16, cnt int) ([]uint16, error) {
-	result := make([]uint16, 0, cnt)
+	log.Printf("Call function 0x03 (read holding registers), addr: %d, cnt: %d", addr, cnt)
 
+	result := make([]uint16, 0, cnt)
 	for a := addr; a < addr+uint16(cnt); a++ {
 		reg, err := h.service.GetHoldingRegister(a)
 		if err != nil {
+			log.Printf("Could not get holding register at addr: %d, reason: %v", addr, err)
 			return nil, fmt.Errorf("get register at address %d: %w", a, err)
 		}
 
 		result = append(result, reg)
 	}
 
+	log.Printf("Successfuly read %d holding registers at addr: %d", cnt, addr)
 	return result, nil
 }
 
 func (h *ModbusHandler) ReadInputRegisters0x04(addr uint16, cnt int) ([]uint16, error) {
-	result := make([]uint16, 0, cnt)
+	log.Printf("Call function 0x04 (read input registers), addr: %d, cnt: %d", addr, cnt)
 
+	result := make([]uint16, 0, cnt)
 	for a := addr; a < addr+uint16(cnt); a++ {
 		inputReg, err := h.service.GetInputRegister(a)
 		if err != nil {
+			log.Printf("Could not get input register at addr: %d, reason: %v", addr, err)
 			return nil, fmt.Errorf("get input register at address %d: %w", a, err)
 		}
 
 		result = append(result, inputReg)
 	}
 
+	log.Printf("Successfuly read %d input registers at addr: %d", cnt, addr)
 	return result, nil
 }
 
 func (h *ModbusHandler) WriteSingleCoil0x05(addr uint16, value bool) error {
+	log.Printf("Call function 0x05 (write single coil), addr: %d, value: %v", addr, value)
+
 	if err := h.service.SetCoil(addr, value); err != nil {
+		log.Printf("Could not write coil at addr: %d, reason: %v", addr, err)
 		return fmt.Errorf("set coil at address %d: %w", addr, err)
 	}
 
+	log.Printf("Successfuly written %v to coil at addr: %d", value, addr)
 	return nil
 }
 
 func (h *ModbusHandler) WriteSingleRegister0x06(addr uint16, value uint16) error {
+	log.Printf("Call function 0x06 (write single register), addr: %d, value: %d", addr, value)
+
 	if err := h.service.SetHoldingRegister(addr, value); err != nil {
+		log.Printf("Could not write register at addr: %d, reason: %v", addr, err)
 		return fmt.Errorf("set register at address %d: %w", addr, err)
 	}
 
+	log.Printf("Successfuly written %v to register at addr: %d", value, addr)
 	return nil
 }
 
 func (h *ModbusHandler) WriteMultipleRegisters0x10(addr uint16, values []uint16) error {
+	log.Printf("Call function 0x10 (write multiple registers), addr: %d, values: %v", addr, values)
+
 	for i, value := range values {
 		a := addr + uint16(i)
 
 		if err := h.service.SetHoldingRegister(a, value); err != nil {
+			log.Printf("Could not write multple registers at addr: %d, reason: %v", addr, err)
 			return fmt.Errorf("set register at address %d: %w", a, err)
 		}
 	}
 
+	log.Printf("Successfuly written %v to registers at addr: %d", values, addr)
 	return nil
 }
 
 func (h *ModbusHandler) WriteMultipleCoils0x0F(addr uint16, coils []bool) error {
+	log.Printf("Call function 0x0F (write multiple coils), addr: %d, values: %v", addr, coils)
+
 	for i, coil := range coils {
 		a := addr + uint16(i)
 
 		if err := h.service.SetCoil(a, coil); err != nil {
+			log.Printf("Could not write coils at addr: %d, reason: %v", addr, err)
 			return fmt.Errorf("set coil at address %d: %w", a, err)
 		}
 	}
 
+	log.Printf("Successfuly written %v to coils at addr: %d", coils, addr)
 	return nil
 }
